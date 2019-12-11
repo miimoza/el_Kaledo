5b052de6444b2985a5e2ab20ef17532bda0342cc
Antoine Coulon <antoine.coulon@epita.fr>
2019-12-11 03:10:09 +0100

    sadkl

--- el_kaledo.py
+++ el_kaledo.py
@@ -25,14 +25,8 @@ def main():
 	GPIO.setmode(GPIO.BCM)
 
 
-	switch_on(SWITCH_WATERPUMP, 0.2, 1)
-	GPIO.setup(ALIM5V_WATERPUMP, GPIO.OUT)
-	GPIO.output(ALIM5V_WATERPUMP, GPIO.HIGH)
-	switch_on(SWITCH_OXYGENTANK, 0.4, 1)
-
-	switch_on(SWITCH_WATERPUMP, 0.1, 1)
-	switch_on(SWITCH_WATERPUMP, 0.1, 1)
-	switch_on(SWITCH_WATERPUMP, 0.1, 1)
+	switch_on(SWITCH_WATERPUMP, 1, 1)
+	switch_on(SWITCH_OXYGENTANK, 1, 1)
 
 
 	GPIO.cleanup()
