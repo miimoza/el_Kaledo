0cc2ab53b2b7f285ede577244b10fddf47e49a5c
Antoine Coulon <antoine.coulon@epita.fr>
2019-12-11 03:36:02 +0100

    sadkl

--- el_kaledo.py
+++ el_kaledo.py
@@ -2,14 +2,13 @@ import RPi.GPIO as GPIO
 import time
 
 
-def switch_on(pin, t, gpio_out):
-	if gpio_out:
-		GPIO.setup(pin, GPIO.OUT)
-	else:
-		GPIO.setup(pin, GPIO.IN)
+def switch_on(pin, t):
+	GPIO.setup(pin, GPIO.OUT)
 
 	GPIO.output(pin, GPIO.HIGH)
 	time.sleep(t)
+
+	GPIO.setup(pin, GPIO.IN)
 
 def main():
 	SWITCH_WATERPUMP = 21
@@ -23,17 +22,12 @@ def main():
 
 	GPIO.setmode(GPIO.BCM)
 
-	GPIO.setup(GROUND_WATERPUMP, GPIO.IN)
-	GPIO.setup(ALIM5V_WATERPUMP, GPIO.OUT)
-	GPIO.output(ALIM5V_WATERPUMP, GPIO.LOW)
-	switch_on(SWITCH_WATERPUMP, 1, 1)
-	GPIO.output(ALIM5V_WATERPUMP, GPIO.HIGH)
+	switch_on(SWITCH_WATERPUMP, 1)
 
 
-	switch_on(SWITCH_OXYGENTANK, 1, 1)
+	switch_on(SWITCH_OXYGENTANK, 1)
 
 
-	GPIO.cleanup()
 
 
 	time.sleep(2)
