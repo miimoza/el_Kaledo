711d2330c3f9da729220ef8232335680bb554dea
Antoine Coulon <antoine.coulon@epita.fr>
2019-12-11 02:38:48 +0100

    sadkl

--- el_kaledo.py
+++ el_kaledo.py
@@ -8,12 +8,16 @@ def main():
 	GROUND_WATERPUMP = 6
 
 	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(SWITCH_WATERPUMP, GPIO.OUT)
 
-	GPIO.output(SWITCH_WATERPUMP, GPIO.HIGH)
-	time.sleep(1)
+	GPIO.setup(SWITCH_WATERPUMP, GPIO.OUT)
 	GPIO.output(SWITCH_WATERPUMP, GPIO.LOW)
+	time.sleep(1)
+	GPIO.setup(SWITCH_WATERPUMP, GPIO.OUT)
+	GPIO.output(SWITCH_WATERPUMP, GPIO.HIGH)
 
 
 
 	GPIO.cleanup()
+
+
+	time.sleep(2)
