89a6ba072bbd58cbeb1e4ed56602f62c3a19fdc6
Antoine Coulon <antoine.coulon@epita.fr>
2019-12-13 18:05:59 +0100

    sad

--- thermo.py
+++ thermo.py
@@ -1,5 +1,12 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(1, GPIO.OUT)
 GPIO.output(1, GPIO.LOW)
+
+time.sleep(3)
+
+GPIO.setup(1, GPIO.IN)
+
+GPIO.output(1, GPIO.HIGH)
