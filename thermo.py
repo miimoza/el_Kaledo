057cfaa7d9d292132e85459829c20181733fbd23
Antoine Coulon <antoine.coulon@epita.fr>
2019-12-13 18:25:08 +0100

    sad

--- thermo.py
+++ thermo.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(1, GPIO.OUT)
-GPIO.output(1, GPIO.LOW)
 
-time.sleep(3)
 
-GPIO.setup(1, GPIO.IN)
-
-GPIO.output(1, GPIO.HIGH)
+while True:
+	GPIO.setup(1, GPIO.OUT)
+	GPIO.output(1, GPIO.LOW)
+	time.sleep(3)
+	GPIO.setup(1, GPIO.IN)
+	GPIO.output(1, GPIO.HIGH)
